19030631ed8a47234b2c0d7cd75c0f52c18efab7
69PercentFat <shubham11501@gmail.com>
2023-12-19 15:51:41 +0530

    doesnt work

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Game Constants & Variables
-let inputDir = {x: 0, y: 0}; 
+let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
@@ -8,142 +8,129 @@ let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
-    {x: 13, y: 15}
+    { x: 13, y: 15 }
 ];
 
-food = {x: 6, y: 7};
+let food = { x: 6, y: 7 };
+let board;
+
+
+let hiscoreval;
 
 // Game Functions
 function main(ctime) {
     window.requestAnimationFrame(main);
-    // console.log(ctime)
-    if((ctime - lastPaintTime)/1000 < 1/speed){
+
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
+
     lastPaintTime = ctime;
     gameEngine();
 }
 
 function isCollide(snake) {
-    // If you bump into yourself 
     for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             return true;
         }
     }
-    // If you bump into the wall
-    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+
+    if (snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0) {
         return true;
     }
-        
+
     return false;
 }
 
-function gameEngine(){
-    // Part 1: Updating the snake array & Food
-    if(isCollide(snakeArr)){
-        gameOverSound.play();
-        musicSound.pause();
-        inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
-        snakeArr = [{x: 13, y: 15}];
-        musicSound.play();
-        score = 0; 
-    }
-
-    // If you have eaten the food, increment the score and regenerate the food
-    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
-        foodSound.play();
-        score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
-        }
-        scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
-        let a = 2;
-        let b = 16;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
-    }
-
-    // Moving the snake
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};
-    }
-
-    snakeArr[0].x += inputDir.x;
-    snakeArr[0].y += inputDir.y;
-
-    // Part 2: Display the snake and Food
-    // Display the snake
-    board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
-        snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = e.y;
-        snakeElement.style.gridColumnStart = e.x;
-
-        if(index === 0){
-            snakeElement.classList.add('head');
-        }
-        else{
-            snakeElement.classList.add('snake');
-        }
-        board.appendChild(snakeElement);
-    });
-    // Display the food
-    foodElement = document.createElement('div');
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food')
-    board.appendChild(foodElement);
-
-
+function gameOver() {
+    gameOverSound.play();
+    musicSound.pause();
+    inputDir = { x: 0, y: 0 };
+    // Display a game over message and restart button
+    board.innerHTML = `<div class="game-over">Game Over<br>Score: ${score}<br><button onclick="restartGame()">Restart</button></div>`;
 }
 
+function restartGame() {
+    snakeArr = [{ x: 13, y: 15 }];
+    score = 0;
+    scoreBox.innerHTML = "Score: 0";
+    musicSound.play();
+    window.requestAnimationFrame(main);
+}
+
+function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    if (!touchStartX || !touchStartY) return;
+
+    let touchEndX = event.touches[0].clientX;
+    let touchEndY = event.touches[0].clientY;
+
+    let deltaX = touchEndX - touchStartX;
+    let deltaY = touchEndY - touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Horizontal swipe
+        inputDir.x = deltaX > 0 ? 1 : -1;
+        inputDir.y = 0;
+    } else {
+        // Vertical swipe
+        inputDir.x = 0;
+        inputDir.y = deltaY > 0 ? 1 : -1;
+    }
+
+    touchStartX = 0;
+    touchStartY = 0;
+}
 
 // Main logic starts here
 musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
-if(hiscore === null){
+if (hiscore === null) {
     hiscoreval = 0;
     localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
-}
-else{
+} else {
     hiscoreval = JSON.parse(hiscore);
     hiscoreBox.innerHTML = "HiScore: " + hiscore;
 }
 
 window.requestAnimationFrame(main);
-window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1} // Start the game
+window.addEventListener('keydown', e => {
+    inputDir = { x: 0, y: 1 }; // Start the game
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp");
             inputDir.x = 0;
             inputDir.y = -1;
             break;
 
         case "ArrowDown":
-            console.log("ArrowDown");
             inputDir.x = 0;
             inputDir.y = 1;
             break;
 
         case "ArrowLeft":
-            console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
             break;
 
         case "ArrowRight":
-            console.log("ArrowRight");
             inputDir.x = 1;
             inputDir.y = 0;
             break;
+
         default:
             break;
     }
-
 });
+
+// Add touch events for mobile devices
+let touchStartX = 0;
+let touchStartY = 0;
+
+window.addEventListener('touchstart', handleTouchStart, false);
+window.addEventListener('touchmove', handleTouchMove, false);
